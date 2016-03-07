78363afd56d5ff444e853003d11ec433a5a2dcae
ubuntu <dtox@163.com>
2016-03-07 19:02:36 +0800

    unit test 3

--- vedit_script/test/test_video_script.cpp
+++ vedit_script/test/test_video_script.cpp
@@ -5,7 +5,9 @@ using namespace vedit;
 
 int main(int argc, char* argv[])
 {
+    assert(mlt_factory_init(NULL));
 	Vm::load_script_dir(argv[1]);
 	json_t* compile_value = Vm::call_script(argv[2], vedit::VIDEO_RESOURCE_SCRIPT, NULL);
 	json_dumpf(compile_value, stdout, 0);
+    mlt_factory_close();
 }
